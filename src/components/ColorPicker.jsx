4d944e3141fb0f5accc5033adf26222d80853b27
CodeskStudio <code@codesk.studio>
2024-11-09 13:03:22 +0100

    simplified ColorPicker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,27 +1,11 @@
-// src/components/ColorPicker.js
-import React, { useEffect, useState } from 'react';
-import { RgbaColorPicker } from "react-colorful";
+// components/ColorPicker.jsx
+import React from 'react';
+import { RgbaColorPicker } from 'react-colorful';
 
-const ColorPicker = ({ onColorPick, defaultColor }) => {
-  const [color, setColor] = useState({ r: 0, g: 0, b: 0, a: 1.0 });
-
-  const handleColorChange = (e) => {
-    setColor(e);
-    onColorPick(e);
-  };
-
-  useEffect(() => {
-    if (color !== defaultColor) {
-      setColor(defaultColor);
-    }
-  }, [defaultColor, color]);
-  
-
+export default function ColorPicker({ color, setColor }) {
   return (
-    <div className="p-4">
-      <RgbaColorPicker color={color} onChange={handleColorChange} />
+    <div className="flex-1">
+      <RgbaColorPicker color={color} onChange={setColor} />
     </div>
   );
-};
-
-export default ColorPicker;
+}
